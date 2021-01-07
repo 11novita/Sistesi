0358c10b82a97dcf7ffc9c5a3b92cdfe01322d44
unknown <novitahadi11@gmail.com>
2021-01-07 12:33:22 +0800

    Sistesi

---
+++ resources/views/login.blade.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Login User</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+
+<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+
+<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+
+<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+
+<link rel="stylesheet" type="text/css" href="css/util.css">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+<div class="limiter">
+<div class="container-login100" style="background-image: url('images/img-01.jpg');">
+<div class="wrap-login100 p-t-190 p-b-30">
+<form class="login100-form validate-form">
+<div class="login100-form-avatar">
+<img src="images/avatar-01.png" alt="AVATAR">
+</div>
+<span class="login100-form-title p-t-20 p-b-45">
+E-Monitoring
+<p>
+EXI global aplikasi
+</p>
+</span>
+<div class="wrap-input100 validate-input m-b-10" data-validate="Username is required">
+<input class="input100" type="text" name="username" placeholder="Username">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-user"></i>
+</span>
+</div>
+<div class="wrap-input100 validate-input m-b-10" data-validate="Password is required">
+<input class="input100" type="password" name="pass" placeholder="Password">
+<span class="focus-input100"></span>
+<span class="symbol-input100">
+<i class="fa fa-lock"></i>
+</span>
+</div>
+<div class="flex-sb-m w-full p-t-3 p-b-32">
+<div class="contact100-form-checkbox">
+<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+<label class="label-checkbox100" for="ckb1">
+Remember me
+</label>
+</div>
+<div>
+<a href="#" class="txt1">
+Forgot Password?
+</a>
+</div>
+</div> 
+<div class="container-login100-form-btn p-t-10">
+<button class="login100-form-btn">
+Login
+</button>
+</div>
+</form>
+</div>
+</div>
+</div>
+
+
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<script src="vendor/select2/select2.min.js"></script>
+
+<script src="js/main.js"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-23581568-13');
+	</script>
+</body>
+</html>
